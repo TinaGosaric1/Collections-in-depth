d20e17a4323987d87b65bf140275521391a91ff3
Tina Gosarič <tina.gosaric@gorenje.com>
2022-10-28 13:41:33 +0200

    Adding MapProgram class.

---
+++ src/MapProgram.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProgram {
+    public static void main(String[] args) {
+        Map<String, String> languages = new HashMap<>();
+        languages.put("Java", "A compiled high level, object-oriented, platform independent language.");
+        languages.put("Python", "An interpreted, object-oriented, high-level programming language with dynamic semantics.");
+        languages.put("Algol", "An algorithmic language.");
+        languages.put("BASIC", "Beginners All Purposes Symbolic Instruction Code");
+        languages.put("Lisp", "Therein lies madness.");
+
+        System.out.println(languages.get("Java"));
+        languages.put("Java","this course is about java");
+        System.out.println(languages.get("Java")); //the first value is overriden, because we entered the same key
+    }
+}
